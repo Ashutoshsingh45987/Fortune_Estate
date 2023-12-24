3bdf958a53cbd770bac4ab69c53fc312e8d6d16d
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2023-12-24 15:35:26 +0530

    completed sigin page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -69,7 +69,7 @@ export const SignUp = () => {
           disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {loading ? 'Loading...' : 'Sign Up'}
+          {loading ? 'Loading...' : 'Sign in'}
         </button>
 
     </form>
