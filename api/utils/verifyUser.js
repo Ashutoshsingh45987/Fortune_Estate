df583d6f1f527c0eef1bf6c9675f0446a6b1f9a4
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2023-12-26 02:19:56 +0530

    completed user update fuctionality and fixed some errors

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,13 +1,15 @@
 import jwt from 'jsonwebtoken'
+import { ErrorHandler } from './error.js';
+
 
 export const verifyToken= (req,res, next)=>{
 
     const token = req.cookies.access_token;
 
-    if (!token) return next(errorHandler(401, 'Unauthorized'));
+    if (!token) return next(ErrorHandler(401, 'Unauthorized'));
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(errorHandler(403, 'Forbidden'));
+    if (err) return next(ErrorHandler(403, 'Forbidden'));
 
     req.user = user;
     next();
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -67,7 +67,7 @@ export const SignIn = () => {
           disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {loading ? 'Loading...' : 'Sign Up'}
+          {loading ? 'Loading...' : 'Sign in'}
         </button>
         <OAuth/>
     </form>
