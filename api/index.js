e0b1721da5e630ee367e63af3622a3dec4adf42b
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2023-12-26 05:07:33 +0530

    added Create listing Api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -22,6 +23,7 @@ App.listen(3000,()=>{
 
 App.use("/api/user",userRouter);
 App.use('/api/auth',authRouter);
+App.use('/api/listing',listingRouter);
 
 App.use((err,req,res ,next)=>{
     const statusCode= err.statusCode||500;                       //This is a middleware for error handling ..we will use 'next' to use in further
