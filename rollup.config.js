e9624e25d029b44e189f9e2a8edebbd35bd7ca91
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2023-12-30 16:03:26 +0530

    some packages added to rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,10 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import css from 'rollup-plugin-css-only'; // Import the plugin
+import json from '@rollup/plugin-json';
 
 export default {
-  input: 'client/src/main.jsx', // Replace with your entry point file
+  input: 'client/src/App.jsx', // Replace with your entry point file
 
   output: {
     file: 'dist/bundle.js', // Replace with your desired output file
@@ -20,6 +21,7 @@ export default {
       exclude: 'node_modules/**', // Exclude node_modules from babel transformation
     }),
     css({ output: 'dist/bundle.css' }), // Specify output CSS file
+    json(),
     // Add more plugins as needed
   ],
 };
