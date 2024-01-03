341ed72815f9e9abc7fb16ac8fce4ebce4e9eb74
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2024-01-04 04:32:34 +0530

    fixed eeeror

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -41,7 +41,7 @@ export const Home = () => {
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
-        log(error);
+        console.log(error);
       }
     };
     fetchOfferListings();
